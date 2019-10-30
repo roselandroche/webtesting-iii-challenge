ded805ccf07c3d25493637e47f2c9fc5d25ef11b
roselandroche <r.landroche@gmail.com>
2019-10-30 19:18:31 -0400

    Setting up Dashboard testing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,6 +3,22 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
+import "jest-dom/extend-expect";
 
 import Dashboard from './Dashboard';
 
+// clean up after each test, reset DOM etc
+afterEach(rtl.cleanup);
+
+// renders controls
+test('does it render the controls component', () => {
+    const wrapper = rtl.render(<Dashboard />)
+
+    const element = wrapper.findByText(/controls/i)
+
+    wrapper.debug()
+
+    expect(element).toBeTruthy();
+})
+
+// renders display
